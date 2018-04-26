ece1b17938e18adf19e58e8562668ed0025bde95
Colin Russell <colinrussell101@gmail.com>
2018-04-26 12:30:03 -0400

    started to setup NSURLSession

--- W3D4 GithubRepos/ViewController.m
+++ W3D4 GithubRepos/ViewController.m
@@ -16,13 +16,23 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users/lighthouse-labs/repos"]; // 1
+    NSURLRequest *urlRequest = [[NSURLRequest alloc] initWithURL:url]; // 2
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 3
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 4
+    
+    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        
+        
+    }]; // 5
+    
+    [dataTask resume]; // 6
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
